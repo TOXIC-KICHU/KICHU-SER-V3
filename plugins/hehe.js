815f24f6326bfc5a8e0886a12a817313b74ca197
KichuSer <97310543+Itsme-soman@users.noreply.github.com>
2022-04-10 18:30:12 +0530

    New👀!

---
+++ plugins/hehe.js
@@ -0,0 +1,26 @@
+let handler  = async (m, { conn, usedPrefix: _p }) => {
+let info = `
+*Come on, what are you going to do with it?*
+*Here, thanks, I'm so sorry you press y, that's the link*
+*https://github.com/Itsme-soman/KICHU-SER-V3*
+`.trim()
+
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Hehe*', 'status@broadcast')
+}
+handler.help = ['git']
+handler.tags = ['tutor']
+handler.command = /^(git)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
+
+module.exports = handler
+ 
