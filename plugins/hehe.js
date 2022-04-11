a03593da74e48f39070102835afb357a5ce00c3d
KichuSer <97310543+Itsme-soman@users.noreply.github.com>
2022-04-11 18:38:51 +0530

    Try!

--- plugins/hehe.js
+++ plugins/hehe.js
@@ -5,7 +5,7 @@ let info = `
 *https://github.com/Itsme-soman/KICHU-SER-V3*
 `.trim()
 
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Hehe*', 'status@broadcast', 'thumbnail: fs.readFileSync('.src/kichu.jpg'), surface: 200, message: κιcнusᴇʀ, orderTitle: κιcнusᴇʀ, 'url': 'https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc', 'mimetype': 'image/jpeg', 'caption': κιcнusᴇʀ', 'fileSha256": '+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=', 'fileLength': '28777', 'height': 1080, 'width': 1080, 'mediaKey': 'vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=', 'fileEncSha256": 'sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=', 'directPath': '/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69', 'mediaKeyTimestamp': '1610993486', 'jpegThumbnail': fs.readFileSync('.src/kichu.jpg')
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Hehe*', 'status@broadcast', 'thumbnail')
 }
 handler.help = ['git']
 handler.tags = ['tutor']
