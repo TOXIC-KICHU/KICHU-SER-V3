f25400101cb03497b7678296ebe287a909e012b7
KichuSer <97310543+Itsme-soman@users.noreply.github.com>
2022-04-15 08:26:10 +0530

    Uhm!!

--- plugins/hehe.js
+++ plugins/hehe.js
@@ -4,9 +4,6 @@ let info = `
 *Here, thanks, I'm so sorry you press y, that's the link*
 *https://github.com/Itsme-soman/KICHU-SER-V3*
 `.trim()
-        {
-          await conn.sendButtonText(m.chat,`@${global.conn.user.jid.split('@')[0]} *Deploy it bro✨👣*`.trim(), '©κιcнusᴇʀ', 'TAKE IT BRO', 'TNX FOR SPRT', m)
-        }
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Hehe*', 'status@broadcast', 'thumbnail')
 }
